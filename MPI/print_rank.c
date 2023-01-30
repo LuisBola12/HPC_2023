ac419082b79357536c5593106a1896d52d5fcdef
luisbola <luisbola@UbuntuPersonal.myguest.virtualbox.org>
2023-01-30 13:55:59 -0600

    Commit con ejecicio basico de rango en c

--- MPI/print_rank.c
+++ MPI/print_rank.c
@@ -7,7 +7,7 @@ int main(int argc, char** argv) {
   MPI_Init(NULL, NULL);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
-
+  int zero = 0;
   printf("Hello from process %d of %d\n", rank, size);
   
   MPI_Finalize();
